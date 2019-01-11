e01a9538a39a6564ab81cc7a8a92b4a8e00951ad
Yang Liu <liuyang12@focusmedia.cn>
2019-01-11 17:27:24 +0800

    update

--- demo.py
+++ demo.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
         reference_5pts = get_reference_facial_points(
             output_size, inner_padding_factor, outer_padding, default_square)
 
-        dst_img = warp_and_crop_face(raw, facial5points, reference_5pts, crop_size)
+        # dst_img = warp_and_crop_face(raw, facial5points, reference_5pts, crop_size)
+        dst_img = warp_and_crop_face(raw, facial5points)
         cv.imwrite('images/{}_warped.jpg'.format(i), dst_img)
         img = cv.resize(raw, (224, 224))
         cv.imwrite('images/{}_img.jpg'.format(i), img)
