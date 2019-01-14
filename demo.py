19216333ba4d475120959e3adbabd9b43abf8cef
Yang Liu <liuyang12@focusmedia.cn>
2019-01-14 15:51:21 +0800

    update

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 
 from mtcnn.detector import detect_faces
-from warp_and_crop_face import warp_and_crop_face, get_reference_facial_points
+from warp_and_crop_face import get_reference_facial_points, warp_and_crop_face
 
 if __name__ == "__main__":
     for i in range(10):
@@ -13,19 +13,19 @@ if __name__ == "__main__":
         img = Image.open(img_fn).convert('RGB')
         _, facial5points = detect_faces(img)
         facial5points = np.reshape(facial5points[0], (2, 5))
-        # crop_size = (112, 112)
+        crop_size = (224, 224)
 
-        # default_square = True
-        # inner_padding_factor = 0
-        # outer_padding = (0, 0)
-        # output_size = (112, 112)
+        default_square = True
+        inner_padding_factor = 0.25
+        outer_padding = (0, 0)
+        output_size = (224, 224)
 
         # get the reference 5 landmarks position in the crop settings
-        # reference_5pts = get_reference_facial_points(
-        #     output_size, inner_padding_factor, outer_padding, default_square)
+        reference_5pts = get_reference_facial_points(
+            output_size, inner_padding_factor, outer_padding, default_square)
 
         # dst_img = warp_and_crop_face(raw, facial5points, reference_5pts, crop_size)
-        dst_img = warp_and_crop_face(raw, facial5points)
+        dst_img = warp_and_crop_face(raw, facial5points, reference_pts=reference_5pts, crop_size=crop_size)
         cv.imwrite('images/{}_warped.jpg'.format(i), dst_img)
         img = cv.resize(raw, (224, 224))
         cv.imwrite('images/{}_img.jpg'.format(i), img)
